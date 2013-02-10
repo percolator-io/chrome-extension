9ac278e12d3a212fb8312e24ea0447f50b08c14b
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2013-02-10 12:21:29 +0400

    page action addon

---
+++ background.js
@@ -0,0 +1,24 @@
+var api_url = "http://localhost:3000/api/links/";
+
+var tabCompleted = function(tabId){
+    chrome.pageAction.show(tabId);
+};
+
+var publishLink = function(url){
+    var params = "link[url]=" + url;
+    var req = new XMLHttpRequest();
+    req.open("POST", api_url, true);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    req.send(params);
+};
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo){
+    if(changeInfo.status=='complete'){
+        tabCompleted(tabId);
+    }
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+    console.log(tab);
+    publishLink(tab.url);
+});
